b148ea70a2ccfa9bc772f27922b5ffce5892cbb3
averman <averman@gmail.com>
2020-11-29 04:21:41 +0700

    fix index

--- build/index.js
+++ build/index.js
@@ -1,10 +1,6 @@
 #!/usr/bin/env node
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-const path_1 = __importDefault(require("path"));
 function main(mode) {
     switch (mode) {
         case 'reconfigure':
@@ -15,6 +11,4 @@ function main(mode) {
     return 0;
 }
 exports.default = main;
-/* istanbul ignore if */
-if (process.argv[1] == path_1.default.join(process.cwd(), "src", "index.ts"))
-    main(process.argv[2]);
+main(process.argv[2]);
